2a62fade68fcdbd1a7caf4e76d475d7b8ebf0a3a
Jeremy <jejedech.63@gmail.com>
2023-06-22 10:24:51 +1000

    adding API/users routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,6 +3,8 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  deleteUser,
+  updateUser
 } = require('../../controllers/userController');
 
 // /api/users
@@ -10,5 +12,11 @@ router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser);
+
+// /api/users
+router.route('/').post(createUser);
+
+// /api/users
+router.route('/').delete(deleteUser);
 
 module.exports = router;
